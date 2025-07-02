3a8cfa73a43b9b5d2ef23a76c04ca5837a403706
Dinesh <dinesh@vasa.com>
2025-07-02 22:06:29 +0530

    error handling

---
+++ array.sh
@@ -0,0 +1,12 @@
+ #!/bin/bash
+
+ MOVIES=("HIT3" "COURT" "PUSHPA2" "LOVE TODAY")
+
+ echo "First movie: ${Movie[0]}"
+ echo "First movie: ${Movie[2]}"
+ echo "First movie: ${Movie[3]}"
+ echo "First movie: ${Movie[4]}"
+
+ echo "All movies: ${Movie[@]}"
+ 
+ 
