60000f9853c143858aace8765f827ed807ad232d
Dinesh <dinesh@vasa.com>
2025-07-03 11:07:04 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+
+ All variables passed to script:$@
+ Number of variables passed:$#
+ script name:$0
+ present working directory:$PWD
+ Home directory is running script:$Home
+ User is running the script:$User
+ PID of the present script:$$
+ PID of the last command running in background:$!
